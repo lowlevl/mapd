496199fd8b27543d429ede01664f0c55af10548e
Maya the bee <hello@unw.re>
2025-07-13 18:28:55 +0200

    mapd-ui: set positions reactively from query params

--- mapd-ui/src/map.rs
+++ mapd-ui/src/map.rs
@@ -1,5 +1,5 @@
 use leptos::prelude::*;
-use leptos_leaflet::prelude::*;
+use leptos_leaflet::{leaflet::LatLng, prelude::*};
 use leptos_meta::Style;
 use leptos_router::hooks::query_signal;
 
@@ -7,27 +7,35 @@ use super::gjson::GeoJson;
 
 #[component]
 pub fn Map() -> impl IntoView {
+    let (map, set_map) = create_map_signal();
+
     let (lat, set_lat) = query_signal("lat");
     let (lng, set_lng) = query_signal("lng");
     let (z, set_z) = query_signal("z");
 
-    let (map, set_map) = create_map_signal();
+    let moving = StoredValue::new_local(false);
+    let events = MapEvents::new().move_end(move |_| {
+        if !*moving.read_value() {
+            let map = map.read();
+            let map = map.as_ref().unwrap();
 
-    let events = MapEvents::new()
-        .move_end(move |_| {
-            if let Some(map) = map.read().as_ref() {
-                set_lat.set(Some(map.get_center().lat()));
-                set_lng.set(Some(map.get_center().lng()));
-            }
-        })
-        .zoom_end(move |_| {
-            if let Some(map) = map.read().as_ref() {
-                set_z.set(Some(map.get_zoom()));
-            }
-        });
+            set_lat.set(Some(map.get_center().lat()));
+            set_lng.set(Some(map.get_center().lng()));
+            set_z.set(Some(map.get_zoom()));
+        }
 
-    let center = || Position::new(lat.get().unwrap_or(48.84), lng.get().unwrap_or(2.34));
-    let zoom = || z.get().unwrap_or(14.0);
+        *moving.write_value() = false;
+    });
+
+    Effect::new(move || {
+        if let Some(map) = map.read().as_ref() {
+            let center = LatLng::new(lat.read().unwrap_or(48.84), lng.read().unwrap_or(2.34));
+            let zoom = z.read().unwrap_or(14.0);
+
+            *moving.write_value() = true;
+            map.set_view(&center, zoom);
+        }
+    });
 
     view! {
       <Style>
@@ -46,12 +54,10 @@ pub fn Map() -> impl IntoView {
 
       <MapContainer
         map=set_map
-        zoom=zoom()
-        center=center()
         events
+        class="map"
         min_zoom=13.0
         zoom_control=false
-        class="map"
       >
         <TileLayer url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png" min_zoom=0.0 max_zoom=20.0 />
         <GeoJson />
